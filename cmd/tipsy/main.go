81e3e07c3a9eb77597027e849bba0f99cb6344c7
Y!an <i@yian.me>
2026-03-12 16:54:45 +0800

    feat: dynamically set version from build info

--- cmd/tipsy/main.go
+++ cmd/tipsy/main.go
@@ -3,10 +3,19 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime/debug"
 )
 
 // 编译时通过 ldflags 注入
 var Version = "dev"
+
+func init() {
+	if Version == "dev" {
+		if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "" && info.Main.Version != "(devel)" {
+			Version = info.Main.Version
+		}
+	}
+}
 
 func main() {
 	if err := rootCmd.Execute(); err != nil {
